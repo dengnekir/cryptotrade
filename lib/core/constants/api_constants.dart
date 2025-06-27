0e64bb154c96bff6e9cbfb87ccedea870a3701b2
Ferhat Uslu <93781530+dengnekir@users.noreply.github.com>
2025-06-27 23:05:38 +0300

    no api

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -1,10 +1,8 @@
 class ApiConstants {
   // Bu değerler kendi API anahtarlarınızla değiştirilmelidir
-  static const String aiAnalysisApiKey = 'YOUR_API_KEY';
-  static const String aiAnalysisApiUrl = 'https://your-api-url.com/analyze';
 
   // Gemini API Sabitleri
-  static const String geminiApiKey = 'AIzaSyACSDFTst4mXYn4x_e1SFa19sKhUtpCRgc';
+  static const String geminiApiKey = '';
   static const String geminiApiUrl =
       'https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent';
   static const String geminiTextUrl =
